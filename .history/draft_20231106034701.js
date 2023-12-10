1ca388225fdf3eaa035d1c28b685388917dcc4b7
omarkamelmostafa <iomarkamel@gmail.com>
2023-12-10 07:24:53 +0200

    Add draft_20231106034701.js

---
+++ .history/draft_20231106034701.js
@@ -0,0 +1,17 @@
+// write an array of numbers
+const array = [1, 2, 3, 4, 5];
+
+const calc = (array) => {
+  const initialValue = 0;
+  // const average = array.forEach((element, key, map) => {
+  // });
+  // console.log(element, key, map);
+  const average = array.reduce((a, b) => a + b, 3) / array.length;
+  // const average = array.reduce(
+  //   (accumulator, currentValue) => accumulator + currentValue,
+  //   0 / array.length
+  // );
+  console.log(average);
+};
+
+calc(array);
