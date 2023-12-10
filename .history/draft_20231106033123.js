49af775b44e812552876db52846d62c9d09a3948
omarkamelmostafa <iomarkamel@gmail.com>
2023-12-10 07:24:41 +0200

    Add draft_20231106033123.js

---
+++ .history/draft_20231106033123.js
@@ -0,0 +1,12 @@
+// write an array of numbers
+const array = [1, 2, 3, 4, 5];
+
+const calc = (array) => {
+  const average = array.forEach((element, key, map) => {
+    clg()
+  });
+return average
+};
+
+
+calc(array);
